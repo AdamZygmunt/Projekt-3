2f37c44199885894b9e68ac863d3ef7bdbe5bf70
AdamZygmunt <adam.zygmunt006@gmail.com>
2022-01-08 18:46:04 +0100

    podpunkt 1 ale póki co poza funkcją

--- main.cpp
+++ main.cpp
@@ -2,6 +2,38 @@
 #include <iomanip>
 
 using namespace std;
+int  licznik = 0;
+void sasiedzi()
+{
+    int n, m, i, j, w1, w2;
+    char ** A;
+
+    for(i=0;i<n;i++)
+    {
+         for(j=0;j<n;j++)
+        {
+            if(A[w1][w2] == 1)
+            {
+                licznik++;
+                cout << "sasiadem wierzcholka w" << i << " jest wierzcholek w" << j << endl;
+            }
+        }
+
+    }
+
+
+
+
+  //  return licznik;
+}
+
+
+
+
+
+
+
+
 
 
 
@@ -12,24 +44,24 @@ int main( )
 
 
   cout << "podaj liczbe wierzcholkow, a nastepnie liczbe krawedzi grafu " << endl;
-  cin >> n >> m;         // Czytamy liczbê wierzcholków i krawedzi
+  cin >> n >> m;         // Czytamy liczbe wierzcholkow i krawedzi
 
-  A = new char * [ n ];  // Tworzymy tablicê wskaŸników
+  A = new char * [ n ];  // Tworzymy tablice wskaznikow
 
   for( i = 0; i < n; i++ )
     A [ i ] = new char [ n ]; // Tworzymy wiersze
 
-  // Macierz wype³niamy zerami
+  // Macierz wypelniamy zerami
 
   for( i = 0; i < n; i++ )
     for( j = 0; j < n; j++ ) A [ i ][ j ] = 0;
 
-  // Odczytujemy kolejne definicje krawêdzi
+  // Odczytujemy kolejne definicje krawedzi
 
   for( i = 0; i < m; i++ )
   {
     cout << "podaj poczatkowy i koncowy wierzcholek krawedzi " << i+1 << endl;
-    cin >> w1 >> w2;    // Wierzcholek startowy i koncowy krawêdzi
+    cin >> w1 >> w2;    // Wierzcholek startowy i koncowy krawedzi
     A [ w1 ][ w2 ] = 1; // Krawedz w1->w2 obecna
   }
 
@@ -54,5 +86,19 @@ int main( )
 
   cout << endl;
 
+  cout << "1) sasiedzi wierzcholkow :" << endl;
+sasiedzi();
+ for(i=0;i<n;i++)
+    {
+         for(j=0;j<n;j++)
+        {
+            if(A[i][j] == 1)
+            {
+                licznik++;
+                cout << "sasiadem wierzcholka w" << i << " jest wierzcholek w" << j << endl;
+            }
+        }
+
+    }
   return 0;
 }
